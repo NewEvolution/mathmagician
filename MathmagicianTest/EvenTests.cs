7c57fb529d15d4ec001bc771ee2f327e59523f11
Ryan Tanay <ryantanay@newevolution.org>
2015-10-23 14:31:09 -0500

    Completed natural numbers implementation with tests

---
+++ MathmagicianTest/EvenTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Mathmagician;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MathmagicianTest
+{
+    [TestClass]
+    public class EvenTests
+    {
+        [TestMethod]
+        public void EnsureICanCreateClassInstance()
+        {
+            EvenNumbers even = new EvenNumbers();
+        }
+    }
+}
