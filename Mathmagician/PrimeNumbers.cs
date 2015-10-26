6e60a5b7e3caafa0390b6c339b1f7b66866e6f02
Ryan Tanay <ryantanay@newevolution.org>
2015-10-26 13:14:53 -0500

    Reordered some usings so that they're pretty-like

--- Mathmagician/PrimeNumbers.cs
+++ Mathmagician/PrimeNumbers.cs
@@ -1,9 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Numerics;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace Mathmagician
 {
